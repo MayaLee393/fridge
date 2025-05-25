59a1b0f0833f2ee0005dbfa024bf565ed5af52a1
MayaLee393 <maya.v.lee393@gmail.com>
2025-05-24 22:04:35 -0500

    initial

---
+++ README.old.md
@@ -0,0 +1,11 @@
+# fridge
+
+What will this app be doing?
+
+- track items you have bought (buy date and exp date)
+- add items to a grocery list
+
+How will we do that?
+
+- data stored in firebase
+- app made with react
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        My fridge
+      </h1>
+    </div>
+  );
+}
+
+export default App;
